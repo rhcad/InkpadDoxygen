c3abc499ab2d4e5cd36e6a2f746c31746724cad1
Zhang Yungui <rhcad@hotmail.com>
2014-04-18 14:58:44 +0800

    Organize source files as Inkpad Xcode project.

--- cp.sh
+++ cp.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-mkdir -p all.tmp/Inkpad
+
+rm -rf all.tmp/Inkpad
+mkdir -p all.tmp/Inkpad/Additions
+mkdir -p all.tmp/Inkpad/Model
+mkdir -p all.tmp/Inkpad/View
+mkdir -p all.tmp/Inkpad/Controllers
+
 cp -R ../Inkpad/Classes  all.tmp/Inkpad
 cp -R ../Inkpad/Inkpad-Core  all.tmp/Inkpad
 cp -R ../Inkpad/Openclipart  all.tmp/Inkpad
+
+mv all.tmp/Inkpad/Classes/*+Additions.* all.tmp/Inkpad/Additions
+mv all.tmp/Inkpad/Classes/*Controller.* all.tmp/Inkpad/Controllers
+mv all.tmp/Inkpad/Classes/*View.* all.tmp/Inkpad/View
+mv all.tmp/Inkpad/Classes/WDActivity* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/WDDocument* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/WDDrawing* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/WDEmail* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/WDFont* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/WD*Font.* all.tmp/Inkpad/Model
+mv all.tmp/Inkpad/Classes/*.* all.tmp/Inkpad/View
